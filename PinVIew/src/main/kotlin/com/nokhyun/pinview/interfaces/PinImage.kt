804be946c76ca4925f3926c318444e1de7123e23
nokhyun90 <nokhyun90@gmail.com>
2023-05-03 23:20:55 +0900

    - 불필요코드 제거

--- PinVIew/src/main/kotlin/com/nokhyun/pinview/interfaces/PinImage.kt
+++ PinVIew/src/main/kotlin/com/nokhyun/pinview/interfaces/PinImage.kt
@@ -1,12 +1,8 @@
 package com.nokhyun.pinview.interfaces
 
-import android.content.Context
 import androidx.annotation.DrawableRes
 
 interface PinImage {
-//    val context: Context
-//    val pinLength: Int
-//    val height: Int
     fun addImage(@DrawableRes img: Int, pinInputSize: Int)
     fun removeImage()
 }
